a1dc2fbcd7fc8bccfac0b95038c7eac426e56046
Anish Pramod <anish.solleti@gmail.com>
2023-08-18 15:03:30 +0530

    gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-text = "this is a sample text  and also works with double space. 12345 121333 1214 21313 "
+text = "this is a sample text  and also works with double space. 12345 121333 1214 21313 . can i  havea  alot more text in this to make it any more vialle to scan a nd paste hand wrting biiiiiiiiiiii. fjdnd"
 
 bg = Image.new(mode="RGBA", size=(595, 842), color = "white")
 im = Image.new(mode="RGBA", size=(10,10), color = "pink")
